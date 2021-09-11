d34c1d3f3eba2eaf7d01926c4bd915bffd021812
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-11 20:09:26 +0530

    Update game.py

--- game.py
+++ game.py
@@ -7,9 +7,9 @@ pygame.init()
 display_width = 800
 display_height = 600
 
-# black = (0,0,0)
-# white = (255,255,255)
-# red = (255,0,0)
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
 
 # block_color = (53,115,255)
 
