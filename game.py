f92047a93fe85aed417f344c519edee2837ed560
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-12 12:59:57 +0530

    Update game.py

--- game.py
+++ game.py
@@ -37,7 +37,7 @@ def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
-# def message_display(text):
+def message_display(text):
 #     largeText = pygame.font.Font('freesansbold.ttf',115)
 #     TextSurf, TextRect = text_objects(text, largeText)
 #     TextRect.center = ((display_width/2),(display_height/2))
